815a127b91623ba29e34a5268f015e4a4db8488d
I-the-Creator <andrey.burluckiy@gmail.com>
2022-10-04 22:56:35 +0200

    ChatContext and AuthContext imported and request to DB added

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useEffect } from 'react'
+import { useRef } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { ChatContext } from '../context/ChatContext'
 
-const Message = () => {
+const Message = ({ message }) => {
+  // console.log(message) // DEBUG
+
+  // handle time
+  var dateObj = message.date.toDate()
+  const hours = dateObj.getHours()
+  const minutes = dateObj.getMinutes()
+
+  const { currentUser } = useContext(AuthContext)
+  const { data } = useContext(ChatContext)
+
+  const ref = useRef()
+
+  // to scroll to the bottom chat window once new message sent
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [message])
+
   return (
-    <div className="message owner">
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && 'owner'}`}
+    >
       <div className="messageInfo">
         <img
-          src="https://images.pexels.com/photos/13586038/pexels-photo-13586038.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.selectedUser.photoURL
+          }
           alt=""
         />
-        <span>just now</span>
+        <span className="time">{`${hours}:${minutes}`}</span>
       </div>
       <div className="messageContent">
-        <p>hello</p>
-        <img
-          src="https://images.pexels.com/photos/13586038/pexels-photo-13586038.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
+        {message.text && <p>{message.text}</p>}
+        {message.img && <img src={message.img} alt="" />}
       </div>
     </div>
   )
