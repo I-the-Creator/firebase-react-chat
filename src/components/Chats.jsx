3412a661859873ad8578d49aacf99e118ddb5302
I-the-Creator <andrey.burluckiy@gmail.com>
2022-10-04 22:56:18 +0200

    ChatContext and AuthContext imported and request to DB added

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,40 +1,54 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { doc, onSnapshot } from 'firebase/firestore'
+
+import { db } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
+import { ChatContext } from '../context/ChatContext'
 
 const Chats = () => {
+  const [chats, setChats] = useState([])
+
+  const { currentUser } = useContext(AuthContext)
+  const { dispatch } = useContext(ChatContext)
+
+  // request to DB to get all user chats for current(logged in) user
+  // firebase onSnapshot method used to get real-time data
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, 'userChats', currentUser.uid), (doc) => {
+      setChats(doc.data())
+    })
+
+    //cleanup
+    return () => {
+      unsub()
+    }
+  }, [currentUser.uid])
+
+  // console.log(Object.entries(chats))  // DEBUG
+
+  const handleSelect = (selectedUser) => {
+    dispatch({ type: 'CHANGE_USER', payload: selectedUser })
+  }
+
   return (
     <div className="chats">
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/13586038/pexels-photo-13586038.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>My latest message</p>
-        </div>
-      </div>
-
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/13586038/pexels-photo-13586038.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>My latest message</p>
-        </div>
-      </div>
-
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/13586038/pexels-photo-13586038.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>My latest message</p>
-        </div>
-      </div>
+      {/* convert returned object - 'chats' -  to an Array of 'chat' objects 
+      and sort it to show the chats in chronological order*/}
+      {Object.entries(chats)
+        ?.sort((a, b) => b[1].date - a[1].date)
+        .map((chat) => (
+          <div
+            className="userChat"
+            key={chat[0]}
+            onClick={() => handleSelect(chat[1].recipientInfo)} // send selected user object as a parameter
+          >
+            <img src={chat[1].recipientInfo.photoURL} alt="" />
+            <div className="userChatInfo">
+              <span>{chat[1].recipientInfo.displayName}</span>
+              <p>{chat[1].lastMessage?.text}</p>
+            </div>
+          </div>
+        ))}
     </div>
   )
 }
