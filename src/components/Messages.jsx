2c6c7bb8fd902bceac71bb5c3a12d3b591f24d78
I-the-Creator <andrey.burluckiy@gmail.com>
2022-10-05 22:58:05 +0200

    ChatContext and AuthContext imported and requests to DB  to get 'chats' collections

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import { doc, onSnapshot } from 'firebase/firestore'
+import React, { useContext, useEffect, useState } from 'react'
+
+import { ChatContext } from '../context/ChatContext'
+import { db } from '../firebase'
 import Message from './Message'
 
 const Messages = () => {
+  const [messages, setMessages] = useState([])
+
+  const { data } = useContext(ChatContext)
+
+  //fetch 'chats' collection form DB to get messages for selected chat
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, 'chats', data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages)
+    })
+
+    return () => {
+      unsub()
+    }
+  }, [data.chatId])
+
+  // console.log(messages) // DEBUG
+
   return (
     <div className="messages">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {messages.map((message) => (
+        <Message message={message} key={message.id} />
+      ))}
     </div>
   )
 }
